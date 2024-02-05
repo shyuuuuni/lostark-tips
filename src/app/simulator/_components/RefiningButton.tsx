8984def49d06293a72ea2236ed243810509ee688
Seung-hyun Kim <seunghyun4525@gmail.com>
2024-02-06 07:44:41 +0900

    style: 상급 재련 버튼 스타일 변경

--- src/app/simulator/_components/RefiningButton.tsx
+++ src/app/simulator/_components/RefiningButton.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import styles from './refiningButton.module.css';
+
 import { useSimulatorStore } from '@/app/simulator/_stores/useSimulatorStore';
 import { MouseEventHandler } from 'react';
 import { useThrottle } from '@/app/_hooks/useThrottle';
@@ -8,20 +10,16 @@ export default function RefiningButton() {
   const isMaxLevel = useSimulatorStore((store) => store.isMaxLevel);
   const [exp, setExp] = useSimulatorStore((store) => [store.exp, store.setExp]);
 
-  const onClick: MouseEventHandler<HTMLButtonElement> = () => {
-    console.log('click');
+  const onClick: MouseEventHandler<HTMLDivElement> = useThrottle(() => {
     if (isMaxLevel()) {
       return;
     }
     setExp(exp + 30);
-  };
-
-  const throttledOnClick = useThrottle(onClick, 500 + 50);
+  }, 500 + 50);
 
   return (
-    <>
-      <button onClick={throttledOnClick}>상급 재련</button>
-      <button onClick={() => setExp(exp + 1000)}>상급 재련(+1000)</button>
-    </>
+    <div className={styles.container} onClick={onClick}>
+      상급 재련
+    </div>
   );
 }
