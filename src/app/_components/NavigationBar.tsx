208494e034081f64b16e758ca464f42878dae806
Seung-hyun Kim <seunghyun4525@gmail.com>
2024-03-03 15:54:27 +0900

    feat: 문의하기 버튼에 디스코드 ID 추가

--- src/app/_components/NavigationBar.tsx
+++ src/app/_components/NavigationBar.tsx
@@ -6,9 +6,27 @@ import Image from 'next/image';
 import Logo from '../../../public/logo.svg';
 import { useSelectedLayoutSegment } from 'next/navigation';
 import clsx from 'clsx';
+import withTooltipPortal from '@/app/_hocs/withTooltipPortal';
+import { Tooltip } from 'react-tooltip';
+import { useState } from 'react';
+
+const PortalTooltip = withTooltipPortal(Tooltip);
 
 export default function NavigationBar() {
   const segment = useSelectedLayoutSegment();
+  const [copied, setCopied] = useState(false);
+
+  const onClickTootip = () => {
+    const TEXT = '.shyuuni';
+
+    navigator.clipboard
+      .writeText(TEXT)
+      .then(() => {
+        setCopied(true);
+        setTimeout(() => setCopied(false), 1500);
+      })
+      .catch(() => setCopied(false));
+  };
 
   return (
     <nav className={styles.navbar}>
@@ -30,8 +48,26 @@ export default function NavigationBar() {
         </Link>
       </div>
       <div className={styles.rightNavZone}>
-        <div className={styles.inquireBtn}>문의하기</div>
-        <div>더보기</div>
+        <div className={styles.inquireBtn} data-tooltip-id="inquire-button">
+          문의하기
+        </div>
+        <PortalTooltip
+          id="inquire-button"
+          place={'bottom-end'}
+          openOnClick={true}
+          clickable={true}
+        >
+          <div className={styles.tooltip} onClick={onClickTootip}>
+            {copied ? (
+              <span>복사됨</span>
+            ) : (
+              <>
+                <span>디스코드</span>
+                <span>.shyuuni</span>
+              </>
+            )}
+          </div>
+        </PortalTooltip>
       </div>
     </nav>
   );
