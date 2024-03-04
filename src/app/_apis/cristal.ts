53005efb3abdf60876e811e8ffef8f4d5901c4e5
Seung-hyun Kim <seunghyun4525@gmail.com>
2024-03-04 14:27:52 +0900

    feat: 크리스탈 시세 API 구현

---
+++ src/app/_apis/cristal.ts
@@ -0,0 +1,16 @@
+import { CristalApiResponse } from '@/type/cristal';
+import { findClosestCristalPrice } from '@/app/_lib/cristal';
+
+// 금일 00:00 기준 크리스탈 시세 가져오기
+export const fetchCristalPrice = async () => {
+  const res = await fetch(
+    'https://loatool.taeu.kr/api/crystal-history/ohlc/1d',
+    // 외부 API 호출이기 때문에 revalidate 기간을 늘림
+    { next: { tags: ['cristal', 'price'], revalidate: 36000 } },
+  );
+  const data = (await res.json()) as CristalApiResponse[];
+  const closestData = findClosestCristalPrice(data);
+
+  // 상한가와 하한가 평균으로 계싼
+  return Math.floor((closestData.high + closestData.low) / 2);
+};
---
+++ src/app/_lib/cristal.ts
@@ -0,0 +1,23 @@
+import { CristalApiResponse } from '@/type/cristal';
+import dayjs from 'dayjs';
+
+export const findClosestCristalPrice = (
+  cristalPrices: CristalApiResponse[],
+): CristalApiResponse => {
+  const today = dayjs();
+
+  let closest = cristalPrices[0],
+    closestDiff = Infinity;
+
+  for (const price of cristalPrices) {
+    const priceDate = dayjs(price.dt);
+    const diff = Math.abs(today.diff(priceDate, 'second'));
+
+    if (diff <= closestDiff) {
+      closest = price;
+      closestDiff = diff;
+    }
+  }
+
+  return closest;
+};
